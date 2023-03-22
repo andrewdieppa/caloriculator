07b0fa938231f6829b15c25c3728ba34f4715404
Andrew Dieppa <9542620+andrewdieppa@users.noreply.github.com>
2023-03-21 18:03:00 -0700

    add dot badges to edit bar macro buttons

--- src/components/Meals/EditButtonBar.js
+++ src/components/Meals/EditButtonBar.js
@@ -1,14 +1,17 @@
-import { Button, Paper, Stack } from '@mui/material';
+import { Button, Paper, Stack, Badge } from '@mui/material';
 import {
   toggleProteinModal,
   toggleCarbModal,
   toggleFatModal,
   toggleArrangeModal,
 } from '../../store/uiSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const EditButtonBar = () => {
   const dispatch = useDispatch();
+  const { proteinPercTotal, carbPercTotal, fatPercTotal } = useSelector(
+    state => state.mealsData
+  );
 
   return (
     <Paper>
@@ -16,27 +19,33 @@ const EditButtonBar = () => {
         direction="row"
         sx={{ mb: 2, p: 1, justifyContent: 'space-between' }}
       >
-        <Button
-          variant="contained"
-          size="small"
-          onClick={() => dispatch(toggleProteinModal())}
-        >
-          Protein
-        </Button>
-        <Button
-          variant="contained"
-          size="small"
-          onClick={() => dispatch(toggleCarbModal())}
-        >
-          Carbs
-        </Button>
-        <Button
-          variant="contained"
-          size="small"
-          onClick={() => dispatch(toggleFatModal())}
-        >
-          Fat
-        </Button>
+        <Badge variant="dot" color="error" invisible={proteinPercTotal === 100}>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => dispatch(toggleProteinModal())}
+          >
+            Protein
+          </Button>
+        </Badge>
+        <Badge variant="dot" color="error" invisible={carbPercTotal === 100}>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => dispatch(toggleCarbModal())}
+          >
+            Carbs
+          </Button>
+        </Badge>
+        <Badge variant="dot" color="error" invisible={fatPercTotal === 100}>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => dispatch(toggleFatModal())}
+          >
+            Fat
+          </Button>
+        </Badge>
         <Button
           variant="contained"
           size="small"
